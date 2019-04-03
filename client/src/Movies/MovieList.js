0fcad0e00ebb89242a1bb25c0cb42666ae755f91
jjgorton <jjgorton@gmail.com>
2019-04-03 18:13:31 -0400

    both HTTP requests working now

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -3,33 +3,37 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import MovieCard from './MovieCard';
 export default class MovieList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			movies : []
+		};
+	}
 
-  componentDidMount() {
-    // fill me in with an HTTP Request to `localhost:5000/api/movies`
-    this.setState({ movies: [] });
-  }
+	componentDidMount() {
+		// fill me in with an HTTP Request to `localhost:5000/api/movies`
+		axios
+			.get(`http://localhost:5000/api/movies`)
+			.then((res) => {
+				console.log(res);
+				this.setState({ movies: res.data });
+			})
+			.catch((err) => console.log(err));
+	}
 
-  render() {
-    return (
-      <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
-        ))}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="movie-list">
+				{this.state.movies.map((movie) => <MovieDetails key={movie.id} movie={movie} />)}
+			</div>
+		);
+	}
 }
 
 function MovieDetails({ movie }) {
-  return (
-    <Link to={`/movies/${movie.id}`}>
-      <MovieCard movie={movie} />
-    </Link>
-  );
+	return (
+		<Link to={`/movies/${movie.id}`}>
+			<MovieCard movie={movie} />
+		</Link>
+	);
 }
